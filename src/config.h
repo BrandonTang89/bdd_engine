0b02ead3235311285a687157bcffd1428d52d22c
Brandon Tang <tangyuhanbrandon@gmail.com>
2025-05-23 18:27:21 +0100

    Add proper lexer errors. Implement BDD ids as valid identifiers

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #include <absl/base/log_severity.h>
 
 constexpr bool echo_input = false;    // Set to true to echo input
-constexpr bool print_tokens = true;  // Set to true to print tokens
+constexpr bool print_tokens = false;  // Set to true to print tokens
 constexpr bool print_ast = false;     // Set to true to print AST
 constexpr auto warning_level = absl::LogSeverity::kWarning;
 
