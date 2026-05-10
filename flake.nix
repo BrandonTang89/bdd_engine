b988ce3ad10fe79dcb5a962fef0ecaf5131479e7
BrandonTang89 <tangyuhanbrandon@gmail.com>
2026-05-10 09:59:05 +0100

    Enable nix build

--- flake.nix
+++ flake.nix
@@ -5,22 +5,51 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.gcc14Stdenv.mkDerivation {
+            pname = "bdd_engine";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+            ];
+
+            buildInputs = [
+              pkgs.abseil-cpp
+              pkgs.catch2_3
+            ];
+
+            cmakeFlags = [
+              "-DCMAKE_BUILD_TYPE=Release"
+            ];
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/bdd_engine";
+        };
+      });
+
       devShells = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
         in {
           default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
             packages = with pkgs; [
               gcc14
-              cmake
-              ninja
-              abseil-cpp
-              catch2_3
             ];
 
             shellHook = ''
